7c0eb0062f0761feff974f7b6bda00eff07814dc
lucafrancesc <eto.lucafrancesco@gmail.com>
2026-06-24 17:44:03 +0100

    Add basic logic and tests

--- src/main/scala/BillingService/BillingService.scala
+++ src/main/scala/BillingService/BillingService.scala
@@ -2,6 +2,12 @@ package BillingService
 
 object BillingService {
 
-  def calculateBill(itemNames: List[String]): BigDecimal = 0
+  def calculateBill(itemNames: List[String]): BigDecimal = {
+    val itemsFound = itemNames.flatMap { name =>
+      Menu.items.get(name)
+    }
+
+    itemsFound.map(_.price).sum
+  }
 
 }
